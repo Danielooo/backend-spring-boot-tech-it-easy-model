67c80a94c3b63ab233c27a8537b9ccccd2563950
Danielooo <dvanraffe@gmail.com>
2024-06-10 10:20:08 +0200

    second commit

---
+++ src/main/java/org/novi/backendspringboottechiteasymodel/BackendSpringBootTechItEasyModelApplication.java
@@ -0,0 +1,13 @@
+package org.novi.backendspringboottechiteasymodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSpringBootTechItEasyModelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSpringBootTechItEasyModelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/novi/backendspringboottechiteasymodel/BackendSpringBootTechItEasyModelApplicationTests.java
@@ -0,0 +1,13 @@
+package org.novi.backendspringboottechiteasymodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSpringBootTechItEasyModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
